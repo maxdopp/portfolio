0de8e332be6302a09730b1f493189c0171bbb821
Max Dopp <mdopp2005@gmail.com>
2024-09-22 22:17:05 -0400

    added hamburger menu to navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,27 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import React, {useState} from "react";
+import './Navbar.css'
 
 function TopNavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  
   return (
-    <div className='navbar'>
-        <ul>
+    <nav className='navbar'>
+      <div className="logo">Logo</div>
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div className={`menu-toggle ${isOpen ? 'open'  : ''}`}></div>
+      </div>
+        <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
             <li><Link to="/" className="link">Home</Link></li>
             <li><Link to="/about" className="link">About</Link></li>
             <li><Link to="/projects" className="link">Projects</Link></li>
             <li><Link to="/contact" className="link">Contact</Link></li>
         </ul>
-    </div>
+    </nav>
   );
 }
 
